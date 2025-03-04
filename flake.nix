0657b705e1b1d0bd8871eed455d01c2c096fcf7a
BinarySkunk <sillycatluna@gmail.com>
2025-03-04 16:11:13 -0300

    add dev shell as a nix flake

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }: let
+    systems = [ "x86_64-linux" "aarch64" ];
+
+    forEachSystem = f: with nixpkgs.lib; foldAttrs mergeAttrs {}
+      (lists.forEach systems (system:
+        mapAttrs (name: value: { ${system} = value; }) (f system)
+      ));
+  in forEachSystem (system: let
+    pkgs = nixpkgs.legacyPackages.${system};
+  in rec {
+    devShells.default = pkgs.mkShell {
+      inputsFrom = [
+        packages.default
+      ];
+
+      shellHook = ''
+        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath (with pkgs; [
+          wayland
+          libxkbcommon
+          mesa
+          xorg.libX11
+          xorg.libXinerama
+        ])}:$LD_LIBRARY_PATH
+      '';
+    };
+
+    packages.default = pkgs.stdenv.mkDerivation {
+      name  = "vita";
+      pname = "vita";
+
+      src = ./.;
+
+      nativeBuildInputs = with pkgs; [
+        clang
+        cmake
+        ninja
+        pkg-config
+      ];
+
+      # TODO(binaryskunk): i need to make this prettier, lol
+      buildInputs = with pkgs; [ wayland.dev libxkbcommon.dev mesa.dev wayland-protocols xorg.libX11 xorg.libxcb xorg.libXcursor xorg.libXrandr xorg.libXinerama xorg.libXi xorg.libXext xorg.libXi xorg.libXft xorg.libXrender xorg.xcbutil xorg.xcbutilimage xorg.xcbutilkeysyms xorg.xcbutilrenderutil ];
+    };
+  });
+}
+
